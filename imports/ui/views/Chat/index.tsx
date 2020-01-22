156bab88f4b0d8b3c065a1a8e08f83f97b55e70f
Andrew Miracle <me@andrewmiracle.com>
2020-01-22 11:50:51 +0000

    improvements to the chat interface

--- imports/ui/views/Chat/index.tsx
+++ imports/ui/views/Chat/index.tsx
@@ -1,36 +1,71 @@
-import React, {  } from 'react';
-
+import React, { useState } from 'react';
 import { InputGroup, Input, InputRightElement, Button, Box, Heading } from '@chakra-ui/core';
+import styled from '@emotion/styled'
+import Wrapper from '../../Layout';
 
+const InputWrapper = styled.div`
+    position: absolute;
+    bottom: 3px;
+    max-height: 80px;
+    padding: 3px;
+    border-top: 1px solid #ddd;
+    margin: 0;
+`
+
+const Announcement = styled.div`
+    position: absolute;
+    top: 0;
+    border-bottom: 1px solid #ccc;
+`
+const MessageWrapper = styled.div`
+    position: relative;
+`
 
 const Chat: React.FC = () => {
+    const [value, setValue] = useState<string>('')
 
+    const handleChange = (event) => {
+        setValue(event.target.value);
+        console.log(value);
+    }
 
+    const buttonClick = (e) => {
+        // This function executes when user clicks the Button or uses the Enter value
+        e.preventDefault()
+        console.log('I EXECCUTE BUTTON', e, value);
+        // console.log(e)
+    }
     return (
-        <Box p={4}>
-            <Heading as="h4" size="md">Welcome to the Chat Room!</Heading>
-            <br />
-            <div className="hint-box-container ">
-                <div className="hide-hint-button"></div>
-                <div className="hint-box">
-                    <h2>Hint</h2>
-                    <div>You can send messages anonymously or tell us your name. To set your name, just send a message saying "My name is _____" and we'll introduce you!</div>
-                </div>
-            </div>
-            <br />
-            <InputGroup size="md">
-                <Input
-                    pr="4.5rem"
-                    type="text"
-                    placeholder="Type Your Message"
-                />
-                <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm" onClick={() => {
-                        alert('You typed a message');
-                    }}>Send</Button>
-                </InputRightElement>
-            </InputGroup>
-        </Box>
+        <React.Fragment>
+            <MessageWrapper>
+                <Box p={1}>
+                    <Announcement>
+                        <Heading as="h6" size="xs">Welcome to the Chat Room!</Heading>
+                        <div className="hint-box-container ">
+                            <small style={{ lineHeight: '1px' }}>To set your name, just send a message saying "My name is _____" and we'll introduce you!</small>
+                        </div>
+                    </Announcement>
+                </Box>
+            </MessageWrapper>
+
+
+            <InputWrapper>
+                <form onSubmit={buttonClick}>
+                    <InputGroup size="md">
+                        <Input
+                            pr="4.5rem"
+                            type="text"
+                            placeholder="Type Your Message"
+                            onChange={handleChange}
+                        />
+                        <InputRightElement width="4.5rem">
+                            <Button type="submit" h="1.75rem" size="sm" onClick={buttonClick}>Send</Button>
+                        </InputRightElement>
+                    </InputGroup>
+                </form>
+
+            </InputWrapper>
+        </React.Fragment>
     )
 }
 
