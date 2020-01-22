19db4a6b7d1bfb7684d9292cb712b1ade13fb641
Michael Hammond <mickeymond@gmail.com>
2020-01-22 09:29:23 +0000

    Chat Screen Tweaks

--- imports/ui/views/Chat/index.tsx
+++ imports/ui/views/Chat/index.tsx
@@ -1,38 +1,36 @@
-import React, { useState, ChangeEvent, ReactEventHandler } from 'react';
+import React, {  } from 'react';
 
-import Message from './message';
+import { InputGroup, Input, InputRightElement, Button, Box, Heading } from '@chakra-ui/core';
 
 
 const Chat: React.FC = () => {
 
 
     return (
-        <div>
-            <div class="hint-box-container ">
-                <div class="hide-hint-button"></div>
-                <div class="hint-box">
+        <Box p={4}>
+            <Heading as="h4" size="md">Welcome to the Chat Room!</Heading>
+            <br />
+            <div className="hint-box-container ">
+                <div className="hide-hint-button"></div>
+                <div className="hint-box">
                     <h2>Hint</h2>
                     <div>You can send messages anonymously or tell us your name. To set your name, just send a message saying "My name is _____" and we'll introduce you!</div>
                 </div>
             </div>
-
-            <div class="messages-box">
-                <div class="messages-scroll">
-                    <div class="header">
-                        <h1>Welcome to the Chat Room!</h1>
-                    </div>
-                    <div class="messages">
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="new-message-container">
-                <form>
-                    <input type="text" class="message-input" placeholder="Type your message here..."></input><button type="submit">Send</button>
-                </form>
-            </div>
-        </div>
+            <br />
+            <InputGroup size="md">
+                <Input
+                    pr="4.5rem"
+                    type="text"
+                    placeholder="Type Your Message"
+                />
+                <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={() => {
+                        alert('You typed a message');
+                    }}>Send</Button>
+                </InputRightElement>
+            </InputGroup>
+        </Box>
     )
 }
 
